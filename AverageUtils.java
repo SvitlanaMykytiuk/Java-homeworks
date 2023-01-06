5b90f0cf8d25d2a54805b660b2568b494eba3336
SvitlanaMykytiuk <svetlana.mikityuk@gmail.com>
2023-01-06 18:50:42 +0200

    hw2

---
+++ AverageUtils.java
@@ -0,0 +1,11 @@
+public class AverageUtils {
+
+    public static double getAverageOfTwo(double a, double b) {
+        return (a + b) / 2;
+    }
+
+    public static double getAverageOfThree(double a, double b, double c) {
+        return (a + b + c) / 3;
+    }
+
+}
