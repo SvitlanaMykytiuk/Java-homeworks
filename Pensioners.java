97a3a8247e37a2a610aeb77053bade64a8cfc032
SvitlanaMykytiuk <svetlana.mikityuk@gmail.com>
2023-01-09 22:39:45 +0200

    hw3

--- Pensioners.java
+++ Pensioners.java
@@ -2,8 +2,6 @@ public class Pensioners extends Persons {
 
     private double pension;
 
-    private double income = (getAge() - 50) * pension;
-
     public Pensioners(String name, int age, int height, int weight, double pension) {
         super(name, age, height, weight);
         this.pension = pension;
@@ -11,6 +9,6 @@ public class Pensioners extends Persons {
 
     @Override
     public void die() {
-        System.out.println("Этот пенсионер умер, он заработал: " + income);
+        System.out.println("Этот пенсионер умер, он заработал: " + (getAge() - 50) * pension);
     }
 }
